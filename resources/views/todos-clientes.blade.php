eba6300af47f2d6bb3cff5372f873dd10b6032d1
Vinicius Felix <vinicius.fellix@outlook.com.br>
2024-02-17 18:32:32 -0300

    Estrutura de tabelas funcional

--- resources/views/todos-clientes.blade.php
+++ resources/views/todos-clientes.blade.php
@@ -1,3 +1,95 @@
+@php
+  $data = [
+    'page_info' => [
+      'title' => 'Todos os clientes',
+      'button_top' => [
+        'name' => '+ Cadastrar Cliente',
+        'route' => 'cadastrar-cliente.index',
+      ],
+      'search' => [
+        'id' => 'search',
+        'name' => 'search',
+        'placeholder' => 'Pesquisar por Razão/Apelido',
+        'function' => false,
+      ],
+    ],
+    'table' => [
+      'header' => [
+        'Razão Social',
+        'Apelido',
+        'Cidade',
+        'País',
+        'Ações',
+      ],
+      'botoes' => [
+        [
+          'route' => 'editar-cliente.index',
+          'icon' => 'fa fa-pencil',
+          'class' => 'table-action edit',
+          'function' => false,
+        ],
+        [
+          'route' => 'deletar-cliente.index',
+          'icon' => 'fa fa-trash',
+          'class' => 'table-action delete',
+          'function' => false,
+        ]
+      ],
+      'data' => [
+        1 => [
+          'razao_social' => 'Portimus Auto Center LTDA.',
+          'apelido' => 'Alex Portimus',
+          'cidade' => 'São José dos Campos - SP',
+          'pais' => 'Brasil',
+        ],
+        2 => [
+          'razao_social' => 'Portimus Auto Center LTDA.',
+          'apelido' => 'Alex Portimus',
+          'cidade' => 'São José dos Campos - SP',
+          'pais' => 'Brasil',
+        ]
+      ],
+    ],
+  ];
+
+  $data2 = [
+    'page_info' => [
+      'title' => 'Todos os motoristas',
+      'button_top' => [
+        'name' => '+ Cadastrar Motorista',
+        'route' => 'cadastrar-motorista.index',
+      ],
+      'search' => [
+        'id' => 'search',
+        'name' => 'search',
+        'placeholder' => 'Pesquisar por Nome/Apelido',
+      ],
+    ],
+    'table' => [
+      'header' => [
+        'Nome',
+        'Empresa',
+        'Cidade',
+        'Estado',
+        'Status',
+        'Ações',
+      ],
+      'data' => [
+        'nome' => 'Henrique Bruno Oliveira',
+        'empresa' => 'Autônomo',
+        'cidade' => 'São José dos Campos',
+        'estado' => 'SP',
+        'status' => 'Ativo',
+        'botoes' => [
+          'id' => 1,
+          'cadastrar-motorista',
+          'editar-motorista',
+        ]
+      ]
+    ]
+  ];
+@endphp
+
 @extends('layouts.user_type.auth')
 
 @section('content')
@@ -7,50 +99,66 @@
       <div class="row">
         <div class="col-12">
           <div class="card mb-4">
-            <div class="card-header pb-0">
-              <h6>Tod</h6>
+            <div class="row align-items-center mb-4">
+              <!-- TÍTULO TABELA -->
+              <div class="col-lg-6">
+                <div class="card-header pb-0">
+                  <h6>Todos os clientes</h6>
+                </div>
+              </div>
+              <!-- FIM TÍTULO TABELA -->
+              <!-- BOTÃO CADASTRAR -->
+              <div class="col-lg-6 text-end">
+                <div class="card-header pb-0">
+                  <a href="#" class="btn bg-gradient-info mt-4 mb-0">+ Cadastrar Cliente</a>
+                </div>
+              </div>
+              <!-- FIM BOTÃO CADASTRAR -->
+              <div class="col-lg-6">
+                <div class="card-header pb-0 pt-0">
+                  <a href="#" class="btn bg-dark mt-4 mb-0 text-white"><i class="fa fa-bars"></i> FILTROS</a>
+                </div>
+              </div>
+              <!-- FIM TÍTULO TABELA -->
+              <!-- BOTÃO CADASTRAR -->
+              <div class="col-lg-6 text-end">
+                <div class="card-header pb-0">
+                  <input class="form-control" type="text" placeholder="Pesquisar por Razão/Apelido" id="search" name="search" onfocus="focused(this)" onfocusout="defocused(this)">
+                </div>
+              </div>
+              <!-- FIM BOTÃO CADASTRAR -->
             </div>
             <div class="card-body px-0 pt-0 pb-2">
               <div class="table-responsive p-0">
                 <table class="table align-items-center mb-0">
+                  <!-- TOPO TABELA -->
                   <thead>
                     <tr>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Razão Social</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Apelido</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Cidade</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">País</th>
-                      <th class="text-secondary opacity-7">#</th>
+                      @foreach($data['table']['header'] as $header)
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">{{ $header }}</th>
+                      @endforeach
                     </tr>
                   </thead>
+                  <!-- FIM TOPO TABELA -->
+                  <!-- REGISTROS TABELA -->
                   <tbody>
-                    <tr>
-                      <td>
-                        <div class="d-flex px-2 py-1">
-                          <div class="d-flex flex-column justify-content-center">
-                            <h6 class="mb-0 text-sm">Portimus Auto Center LTDA.</h6>
-                            <p class="text-xs text-secondary mb-0">Portimus Mecânica Automotiva</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>
-                        <h6 class="mb-0 text-sm">Alex Portimus</h6>
-                      </td>
-                      <td>
-                        <h6 class="mb-0 text-sm">São José dos Campos - SP</h6>
-                      </td>
-                      <td>
-                        <h6 class="mb-0 text-sm">Brasil</h6>
-                      </td>
-                      <td>
-                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs me-2 table-action edit" data-toggle="tooltip" data-original-title="Edit user">
-                          <i class="fa fa-pencil" aria-hidden="true"></i>
-                        </a>
-                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs table-action delete" data-toggle="tooltip" data-original-title="Edit user">
-                          <i class="fa fa-trash" aria-hidden="true"></i>
-                        </a>
-                      </td>
-                    </tr>
+                    @foreach($data['table']['data'] as $key => $client)
+                      <tr>
+                        <td><h6 class="mb-0 text-sm">{{ $client['razao_social'] }}</h6></td>
+                        <td><h6 class="mb-0 text-sm">{{ $client['apelido'] }}</h6></td>
+                        <td><h6 class="mb-0 text-sm">{{ $client['cidade'] }}</h6></td>
+                        <td><h6 class="mb-0 text-sm">{{ $client['pais'] }}</h6></td>
+                        <td>
+                        @foreach($data['table']['botoes'] as $botao)
+                          <a href="" class="text-secondary font-weight-bold text-xs me-2 {{ $botao['class'] }}" data-toggle="tooltip" data-original-title="Edit user">
+                            <i class="{{ $botao['icon'] }}" aria-hidden="true"></i>
+                          </a>
+                        @endforeach
+                        <td>
+                      </tr>
+                    @endforeach
                   </tbody>
+                  <!-- FIM REGISTROS TABELA -->
                 </table>
               </div>
             </div>
