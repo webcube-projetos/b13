bff5a0069ea7222961752fd99cef84970829eeb1
Vinicius Felix <vinicius.fellix@outlook.com.br>
2024-03-21 23:30:55 -0300

    Modal financeiro + Barra de rolagem

--- resources/views/listagem/tableList.blade.php
+++ resources/views/listagem/tableList.blade.php
@@ -57,7 +57,7 @@
         @endforeach
         <td class="text-right">
             @foreach($config->actions as $botao)
-              @if ($botao['onClick'])
+              @if (isset($botao['onClick']))
                 <button 
                     class="button-actions text-secondary font-weight-bold text-xs me-2 {{ $botao['class'] }}" 
                     data-toggle="tooltip" 
@@ -66,7 +66,7 @@
                   >
                     <i class="{{ $botao['icon'] }}" aria-hidden="true"></i>
                 </button>
-                @elseif ($botao['route'])
+                @elseif (isset($botao['route']))
                 <a 
                   href="{{ route($botao['route']) }}"
                   class="button-actions text-secondary font-weight-bold text-xs me-2 {{ $botao['class'] }}" 
@@ -76,6 +76,14 @@
                 >
                   <i class="{{ $botao['icon'] }}" aria-hidden="true"></i>
               </a>
+              @elseif ($botao['name'] === 'modal')
+                <button 
+                  type="button" 
+                  class="button-actions text-secondary font-weight-bold text-xs me-2 {{ $botao['class'] }}" 
+                  data-bs-toggle="modal" 
+                  data-bs-target="#{{ $botao['target'] }}">
+                  <i class="{{ $botao['icon'] }}" aria-hidden="true"></i>
+                </button>
               @endif
             @endforeach
         </td>
@@ -85,5 +93,84 @@
 
   <!-- FIM REGISTROS TABELA -->
 </table>
+
+@if (isset($config['modal']) && $config['modal'])
+<div class="modal fade" id="financeiroModal" tabindex="-1" role="dialog" aria-labelledby="financeiroModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title display-inline-block me-4" id="financeiroModalLabel">Informações Transação</h5>
+        <span id="os_number">O.S. <span class="font-weight-bold">#0831</span></span>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h6>Status de pagamento</h6>
+        <select name="status_payment" id="status_payment" class="form-control mb-4">
+          <option value="NAO_PAGO">Não pago</option>
+          <option value="PAGO">Pago</option>
+        </select>
+
+        <div class="divider mb-3">
+          <div class="financial-info">
+            <p class="font-weight-bold">Tipo:</p>
+            <p class="text-danger font-weight-bold">Saída</p>
+          </div>
+          <div class="financial-info">
+            <p class="font-weight-bold">Serviço:</p>
+            <p class="font-weight-bold">Transfer IN SUV Blindado</p>
+          </div>
+          <div class="financial-info">
+            <p class="font-weight-bold">Cliente:</p>
+            <p class="font-weight-bold">Netflix</p>
+          </div>
+          <hr>
+        </div>
+
+        <div class="divider mb-3">
+          <div class="financial-info">
+            <p class="font-weight-bold">Nome:</p>
+            <p class="font-weight-bold">Juscelino Alencar</p>
+          </div>
+          <div class="financial-info">
+            <p class="font-weight-bold">Banco:</p>
+            <p class="font-weight-bold">Itaú</p>
+          </div>
+          <div class="financial-info">
+            <p class="font-weight-bold">Agência:</p>
+            <p class="font-weight-bold">0001</p>
+          </div>
+          <div class="financial-info">
+            <p class="font-weight-bold">Conta:</p>
+            <p class="font-weight-bold">012345678-9</p>
+          </div>
+          <div class="financial-info">
+            <p class="font-weight-bold">Chave PIX:</p>
+            <p class="font-weight-bold">juscelinoalenc.12@gmail.com</p>
+          </div>
+          <div class="financial-info">
+            <p class="font-weight-bold">Prefere receber:</p>
+            <p class="font-weight-bold">Via PIX</p>
+          </div>
+
+          <div class="financial-info mt-4">
+            <p class="font-weight-bold">Valor: </p>
+            <p class="font-weight-bold text-success">R$800,00</p>
+          </div>
+          <hr>
+        </div>
+      </div>
+      <div class="modal-footer justify-content-between">
+        <div>
+          <h6 class="mb-0 text-sm">Data prevista:</h6>
+          <p class="font-weight-bold">10/04/2024</p>
+        </div>
+        <button type="button" class="btn bg-gradient-primary">Salvar</button>
+      </div>
+    </div>
+  </div>
+</div>
+@endif
 
 {{ $dados->links('components.paginacaoAjax') }}
